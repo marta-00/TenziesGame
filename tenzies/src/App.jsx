e6c952817333b687c9a4e2e6be6aa2c484468112
marta-00 <159717019+marta-00@users.noreply.github.com>
2025-04-17 14:19:54 +0000

    modified:   tenzies/src/App.css 	modified:   tenzies/src/App.jsx 	new file:   tenzies/src/components/Die.jsx 	modified:   tenzies/src/components/Main.jsx

--- tenzies/src/App.jsx
+++ tenzies/src/App.jsx
@@ -2,6 +2,11 @@ import Main from "./components/Main"
 import './App.css'
 
 function App() {
+  function generateAllNewDice() {
+    return new Array(10).fill(0).map(() => ({
+      value: Math.ceil(Math.random() * 6),
+    }))
+  }
 
   return (
     <>
--- tenzies/src/components/Main.jsx
+++ tenzies/src/components/Main.jsx
@@ -1,7 +1,23 @@
+import Die from './Die.jsx'
+
 export default function Main() {
+
     return(
         <main>
-            <div className="active"></div>
+            <div className="active">
+                <div className='container'>
+                    <Die value={3} />
+                    <Die value={2} />
+                    <Die value={4} />
+                    <Die value={6} />
+                    <Die value={1} />
+                    <Die value={3} />
+                    <Die value={5} />
+                    <Die value={2} />
+                    <Die value={4} />
+                    <Die value={6} />
+                </div>
+            </div>
         </main>
     )
 }
